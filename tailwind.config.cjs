2989443fa85b1d00a42acddbd9f88c4b895fb3b7
Seth Randall <sethjrandall@gmail.com>
2022-10-26 20:57:21 -0400

    [ADDED] react-router and tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#EF233C",
+      secondary: "#2B2D42",
+      light: "#EDF2F4",
+      white: "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
